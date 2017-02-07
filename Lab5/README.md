58df053fd5ef1e7cc7d047d0a0dbc34d35cdf558
ilayaraja97 <ilayaraja97@gmail.com>
2017-02-07 02:48:46 +0000

    PINTOS complete

---
+++ Lab5/README.md
@@ -0,0 +1,13 @@
+# Lab 5
+-----
+### Part 1
+* Build and compiled
+* test case alarm-multiple ran sucessfully
+* print-something test case made source is `print-something.c` and entry point is `test_print-something` 
+![execution](1.png)
+
+### Part 2
+* Created thread which sorts a random array of integers using `thread_create()` all which can be observed in the self documented code in the source file `sort-random`.
+* 
+* Used List API to store and sort random values.
+![execution](2.png)
---
+++ Lab5/sort-random.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <kernel/list.h>
+#include "threads/thread.h"
+#include <random.h>
+
+//using list.h
+struct ele{
+	struct list_elem elem;
+    int x;
+}*x;
+
+static bool value_less (const struct list_elem *a_, const struct list_elem *b_, void *aux UNUSED) 
+{
+  const struct ele *a = list_entry (a_, struct ele, elem);
+  const struct ele *b = list_entry (b_, struct ele, elem);
+  return a->x < b->x;
+}
+static void listsort(void * aux UNUSED)
+{
+	thread_set_nice (20);
+	struct list list;
+	x=(struct ele *)malloc(10*sizeof(struct ele));
+	int i;
+	list_init(&list);
+	for(i=0;i<10;i++)
+	{
+		random_bytes(&x[i].x,sizeof(int));
+		list_push_back(&list,&x[i].elem);
+	}
+	
+	list_sort (&list,value_less,NULL);
+	struct list_elem *e;
+	for (e = list_begin (&list); e != list_end (&list);e = list_next (e))
+    {
+          struct ele *f = list_entry (e, struct ele, elem);
+          printf("%d ",f->x);
+    }
+    printf("\n");
+    free(x);
+}
+
+void test_sort_random(void)
+{
+	thread_create("high",PRI_DEFAULT + 3,listsort,NULL);
+}
+
+
